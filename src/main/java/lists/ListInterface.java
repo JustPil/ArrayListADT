2724d89c415d87d71cf48cde7eb7f0fa26eaa25f
JustPil <88640664+JustPil@users.noreply.github.com>
2022-07-13 18:06:28 -0700

    File structure changes, class names, JUNIT tests

---
+++ src/main/java/lists/ListInterface.java
@@ -0,0 +1,17 @@
+package lists;
+
+public interface ListInterface<T> {
+    void add(T t);
+    boolean contains(T t);
+    boolean remove(T t);
+    T get(int index);
+    boolean isEmpty();
+    boolean isFull();
+    int size();
+    void clear();
+    int indexOf(T t);
+    int lastIndexOf(T t);
+    boolean set(int index, T t);
+    int getCapacity();
+    String toString();
+}
